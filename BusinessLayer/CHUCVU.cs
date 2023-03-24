9c37ecbba6dc7cdfd98de5043516de03c8f6253b
Hồ Thị Thuỳ Linh <19574802010066@vinhuni.edu.vn>
2023-03-24 19:36:02 +0700

    Sprint 2

---
+++ BusinessLayer/CHUCVU.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+
+namespace BusinessLayer
+{
+    public class CHUCVU
+    {
+        QuanlynhansuEntities db = new QuanlynhansuEntities();
+        public tb_CHUCVU getItem(int id)
+        {
+            return db.tb_CHUCVU.FirstOrDefault(x => x.IDCV == id);
+        }
+        public List<tb_CHUCVU> getList()
+        {
+            return db.tb_CHUCVU.ToList();
+        }
+        public tb_CHUCVU Add(tb_CHUCVU cv)
+        {
+            try
+            {
+                db.tb_CHUCVU.Add(cv);
+                db.SaveChanges();
+                return cv;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Lỗi: " + ex.Message);
+            }
+        }
+        public tb_CHUCVU Update(tb_CHUCVU cv)
+        {
+            try
+            {
+                var _cv = db.tb_CHUCVU.FirstOrDefault(x => x.IDCV == cv.IDCV);
+                _cv.IDCV = cv.IDCV;
+                db.SaveChanges();
+                return cv;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Lỗi: " + ex.Message);
+            }
+        }
+        public void Delete(int id)
+        {
+
+            try
+            {
+                var _cv = db.tb_CHUCVU.FirstOrDefault(x => x.IDCV == id);
+                db.tb_CHUCVU.Remove(_cv);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Lỗi: " + ex.Message);
+            }
+        }
+    }
+}
