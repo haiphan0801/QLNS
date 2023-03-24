acd19aef6fd4bf56aa0521eb41de84710ba12116
Hồ Thị Thuỳ Linh <19574802010066@vinhuni.edu.vn>
2023-03-24 21:39:13 +0700

    Program

--- QLNhanSu/Program.cs
+++ QLNhanSu/Program.cs
@@ -16,7 +16,7 @@ namespace QLNhanSu
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ChucVu());
+            Application.Run(new MainForm());
         }
     }
 }
