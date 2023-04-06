e76ba913f33611b4892b3ed043b907f510215e7e
Hồ Thị Thuỳ Linh <19574802010066@vinhuni.edu.vn>
2023-04-06 21:27:21 +0700

    Sprint 3 (Hồ Thị Thuỳ Linh)

--- DataLayer/QLNS.Designer.cs
+++ DataLayer/QLNS.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\HEHE\Source\Repos\QLNS1\DataLayer\QLNS.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Admin\Source\Repos\QLNS8\DataLayer\QLNS.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- QLNhanSu/NhanVien.cs
+++ QLNhanSu/NhanVien.cs
@@ -11,6 +11,7 @@ using DataLayer;
 using BusinessLayer;
 using System.IO;
 
+
 namespace QLNhanSu
 {
     public partial class NhanVien : Form
@@ -205,9 +206,180 @@ namespace QLNhanSu
             this.Close();
         }
 
+        public void ExportFile(DataTable dataTable, string sheetName, string title)
+        {
+            Microsoft.Office.Interop.Excel.Application oExcel = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Workbooks oBooks;
+            Microsoft.Office.Interop.Excel.Sheets oSheets;
+            Microsoft.Office.Interop.Excel.Workbook oBook;
+            Microsoft.Office.Interop.Excel.Worksheet oSheet;
+
+            oExcel.Visible = true;
+            oExcel.DisplayAlerts = false;
+            oExcel.Application.SheetsInNewWorkbook = 1;
+            oBooks = oExcel.Workbooks;
+            oBook = (Microsoft.Office.Interop.Excel.Workbook)(oExcel.Workbooks.Add(Type.Missing));
+            oSheets = oBook.Worksheets;
+            oSheet = (Microsoft.Office.Interop.Excel.Worksheet)oSheets.get_Item(1);
+            oSheet.Name = sheetName;
+
+            Microsoft.Office.Interop.Excel.Range head = oSheet.get_Range("A1", "N1");
+            head.MergeCells = true;
+            head.Value2 = title;
+            head.Font.Bold = true;
+            head.Font.Name = "Times New Roman";
+            head.Font.Size = "20";
+            head.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+
+            Microsoft.Office.Interop.Excel.Range cl1 = oSheet.get_Range("A3", "A3");
+            cl1.Value2 = "Mã nhân viên";
+            cl1.ColumnWidth = 12;
+
+            Microsoft.Office.Interop.Excel.Range cl2 = oSheet.get_Range("B3", "B3");
+            cl2.Value2 = "Họ tên nhân viên";
+            cl2.ColumnWidth = 25;
+
+            Microsoft.Office.Interop.Excel.Range cl3 = oSheet.get_Range("C3", "C3");
+            cl3.Value2 = "Giới tính";
+            cl3.ColumnWidth = 18;
+
+            Microsoft.Office.Interop.Excel.Range cl4 = oSheet.get_Range("D3", "D3");
+            cl4.Value2 = "Ngày sinh";
+            cl4.ColumnWidth = 25;
+
+            Microsoft.Office.Interop.Excel.Range cl5 = oSheet.get_Range("E3", "E3");
+            cl5.Value2 = "Điện thoại";
+            cl5.ColumnWidth = 25;
+
+            Microsoft.Office.Interop.Excel.Range cl6 = oSheet.get_Range("F3", "F3");
+            cl6.Value2 = "CCCD";
+            cl6.ColumnWidth = 25;
+
+            Microsoft.Office.Interop.Excel.Range cl7 = oSheet.get_Range("G3", "G3");
+            cl7.Value2 = "Địa chỉ";
+            cl7.ColumnWidth = 35;
+
+            Microsoft.Office.Interop.Excel.Range cl8 = oSheet.get_Range("H3", "H3");
+            cl8.Value2 = "Phòng ban";
+            cl8.ColumnWidth = 20;
+
+            Microsoft.Office.Interop.Excel.Range cl9 = oSheet.get_Range("I3", "I3");
+            cl9.Value2 = "Bộ phận";
+            cl9.ColumnWidth = 20;
+
+            Microsoft.Office.Interop.Excel.Range cl10 = oSheet.get_Range("J3", "J3");
+            cl10.Value2 = "Chức vụ";
+            cl10.ColumnWidth = 20;
+
+            Microsoft.Office.Interop.Excel.Range cl11 = oSheet.get_Range("K3", "K3");
+            cl11.Value2 = "Trình độ";
+            cl11.ColumnWidth = 20;
+
+            Microsoft.Office.Interop.Excel.Range cl12 = oSheet.get_Range("L3", "L3");
+            cl12.Value2 = "Dân tộc";
+            cl12.ColumnWidth = 20;
+
+            Microsoft.Office.Interop.Excel.Range cl13 = oSheet.get_Range("M3", "M3");
+            cl13.Value2 = "Tôn giáo";
+            cl13.ColumnWidth = 20;
+
+            Microsoft.Office.Interop.Excel.Range cl14 = oSheet.get_Range("N3", "N3");
+            cl14.Value2 = "Tên công ty";
+            cl14.ColumnWidth = 20;
+
+
+            Microsoft.Office.Interop.Excel.Range rowHead = oSheet.get_Range("A3", "N3");
+            rowHead.Font.Bold = true;
+
+            rowHead.Borders.LineStyle = Microsoft.Office.Interop.Excel.Constants.xlSolid;
+
+            rowHead.Interior.ColorIndex = 6;
+            rowHead.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+
+            object[,] arr = new object[dataTable.Rows.Count, dataTable.Columns.Count];
+
+            for(int row = 0; row < dataTable.Rows.Count; row++)
+            {
+                DataRow dataRow = dataTable.Rows[row];
+                for (int col = 0; col < dataTable.Columns.Count; col++)
+                {
+                    arr[row, col] = dataRow[col];
+                }
+            }
+
+            int rowStart = 4;
+            int columnStart = 1;
+            int rowEnd = rowStart + dataTable.Rows.Count - 1;
+            int columnEnd = dataTable.Columns.Count;
+
+            Microsoft.Office.Interop.Excel.Range c1 = (Microsoft.Office.Interop.Excel.Range)oSheet.Cells[rowStart, columnStart];
+            Microsoft.Office.Interop.Excel.Range c2 = (Microsoft.Office.Interop.Excel.Range)oSheet.Cells[rowEnd, columnEnd];
+            Microsoft.Office.Interop.Excel.Range range = oSheet.get_Range(c1, c2);
+            range.Value2 = arr;
+
+            range.Borders.LineStyle = Microsoft.Office.Interop.Excel.Constants.xlSolid;
+
+            oSheet.get_Range(c1, c2).HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+        }
+
         private void btnIn_Click(object sender, EventArgs e)
         {
+            DataTable dataTable = new DataTable();
 
+            DataColumn col1 = new DataColumn("MANV");
+            DataColumn col2 = new DataColumn("HOTEN");
+            DataColumn col3 = new DataColumn("GIOITINH");
+            DataColumn col4 = new DataColumn("NGAYSINH");
+            DataColumn col5 = new DataColumn("DIENTHOAI");
+            DataColumn col6 = new DataColumn("CCCD");
+            DataColumn col7 = new DataColumn("DIACHI");
+            DataColumn col9 = new DataColumn("TENPB");
+            DataColumn col10 = new DataColumn("TENBP");
+            DataColumn col11 = new DataColumn("TENCV");
+            DataColumn col12 = new DataColumn("TENTD");
+            DataColumn col13 = new DataColumn("TENDT");
+            DataColumn col14 = new DataColumn("TENTG");
+            DataColumn col15 = new DataColumn("TENCTY");
+
+            dataTable.Columns.Add(col1);
+            dataTable.Columns.Add(col2);
+            dataTable.Columns.Add(col3);
+            dataTable.Columns.Add(col4);
+            dataTable.Columns.Add(col5);
+            dataTable.Columns.Add(col6);
+            dataTable.Columns.Add(col7);
+            dataTable.Columns.Add(col9);
+            dataTable.Columns.Add(col10);
+            dataTable.Columns.Add(col11);
+            dataTable.Columns.Add(col12);
+            dataTable.Columns.Add(col13);
+            dataTable.Columns.Add(col14);
+            dataTable.Columns.Add(col15);
+
+            foreach (DataGridViewRow dtgvRow in dataGridView1.Rows)
+            {
+                DataRow dtrow = dataTable.NewRow();
+
+                dtrow[0] = dtgvRow.Cells[0].Value;
+                dtrow[1] = dtgvRow.Cells[1].Value;
+                dtrow[2] = dtgvRow.Cells[2].Value;
+                dtrow[3] = dtgvRow.Cells[3].Value;
+                dtrow[4] = dtgvRow.Cells[4].Value;
+                dtrow[5] = dtgvRow.Cells[5].Value;
+                dtrow[6] = dtgvRow.Cells[6].Value;
+                dtrow[7] = dtgvRow.Cells[9].Value;
+                dtrow[8] = dtgvRow.Cells[11].Value;
+                dtrow[9] = dtgvRow.Cells[13].Value;
+                dtrow[10] = dtgvRow.Cells[15].Value;
+                dtrow[11] = dtgvRow.Cells[17].Value;
+                dtrow[12] = dtgvRow.Cells[19].Value;
+                dtrow[13] = dtgvRow.Cells[21].Value;
+                
+
+                dataTable.Rows.Add(dtrow);
+            }
+
+            ExportFile(dataTable, "Danh sach", "DANH SÁCH SINH VIÊN");
         }
 
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
